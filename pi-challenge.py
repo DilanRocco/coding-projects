3e40cf1af11d83e2db81c65fbb96960ca32ce53c
dilanrocco <dilan@piscatello.com>
2021-11-13 15:41:25 -0500

    comment changes to pi-challenge

--- pi-challenge.py
+++ pi-challenge.py
@@ -11,6 +11,7 @@ for i in range(iterations):
     #print(xComponent)
     #print(yComponent)
 
+    #gets a value inside a 1 by 1 square 
     pythagValue = math.sqrt(math.pow(xComponent,2) + math.pow(yComponent,2))
 
     #checking if the value is in the unit circle. If it is, add one to the counter.
@@ -18,11 +19,18 @@ for i in range(iterations):
         inUnitCircleCounter += 1;
         
 
-valueOfPi = float((inUnitCircleCounter*4))/iterations
+valueOfPi = float((inUnitCircleCounter)/iterations * 4)
 print("Value of Pi is!!!!")
 print(valueOfPi)  
 
 
+#dervation - number of values in a unit circle / area of one quadrant of a square that
+#sits around the unit circle(1*1) 
 
+#that actually equals 1/4*pir^2. The random distruction essentially equals one fourth of the arrea
+#then you multiple by 4 to get the value of pi
+
+ 
+#this is the branch with the changes. 
 #dervation - pi(r^2)/(2pi)^2 = number of values in circle/number of values in square
 
